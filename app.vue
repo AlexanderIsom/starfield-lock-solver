7076c7a457fd71bb124f1ca83f9341a6a74826a0
Alexander Isom <alexanderdisom@gmail.com>
2023-10-04 10:36:13 +0100

    updated Seo

--- app.vue
+++ app.vue
@@ -3,3 +3,13 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup>
+useSeoMeta({
+  title: 'Starfield lockpick solver',
+  ogTitle: 'Starfield lockpick solver',
+  description: 'A simple ui based tool aimed at solving any lockpick in Starfield from novice to master with a few simple clicks.',
+  ogDescription: 'A simple ui based tool aimed at solving any lockpick in Starfield from novice to master with a few simple clicks.',
+  ogImage: 'https://preview.redd.it/9yfkjym7voza1.png?width=5000&format=png&auto=webp&s=05e94fbbef5e95962895fcf6b6bb13df26517731',
+})
+</script>
